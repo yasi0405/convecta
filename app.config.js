541094c54cb1e5ff643327618a929d42b3607136
Yannick Simon <simon.yannickk@gmail.com>
2025-11-14 18:13:17 +0100

    update android version code to 5

--- app.config.js
+++ app.config.js
@@ -33,6 +33,7 @@ module.exports = ({ config }) => ({
     },
 
     android: {
+      versionCode: 5,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
